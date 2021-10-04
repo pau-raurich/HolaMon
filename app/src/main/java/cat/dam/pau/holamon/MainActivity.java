c77d0ac40cb6441016ef9295fd9f5a8c56122e27
pau-raurich <79652592+pau-raurich@users.noreply.github.com>
2021-10-04 18:36:51 +0200

    Prova

---
+++ app/src/main/java/cat/dam/pau/holamon/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.dam.pau.holamon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
